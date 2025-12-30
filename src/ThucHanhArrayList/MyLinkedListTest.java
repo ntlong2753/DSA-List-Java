444b4aee3b57855c5c359fb7dbbfa52157082a60
nguyenthanhlongvn03 <nguyenthanhlongvn03@gmail.com>
2025-12-30 09:38:33 +0700

    add

--- src/ThucHanhArrayList/MyLinkedListTest.java
+++ src/ThucHanhArrayList/MyLinkedListTest.java
@@ -6,12 +6,12 @@ public class MyLinkedListTest {
     public static void main(String[] args) {
         System.out.println("/=/=/=/= TESTING /=/=/=/=");
         MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(10);
         ll.addFirst(11);
         ll.addFirst(12);
-        ll.addFirst(13);
 
-        ll.add(4,9);
-        ll.add(4,9);
+        ll.add(2,6);
+        ll.add(3,8);
         ll.printList();
     }
 }
